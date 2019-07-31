aaadb7cc4e2d0c98264d2a95dfaeb4e575495d8a
qshuigege <qshuigege@163.com>
2019-07-31 12:41:45 +0800

    啊啊啊

---
+++ test-assembly/src/main/java/com/fs/testassembly/TestAssemblyApplication.java
@@ -0,0 +1,13 @@
+package com.fs.testassembly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAssemblyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAssemblyApplication.class, args);
+    }
+
+}
---
+++ test-assembly/src/test/java/com/fs/testassembly/TestAssemblyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fs.testassembly;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestAssemblyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
