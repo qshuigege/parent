9fad3adb34de244a167f9ae6988b3bbd73df0c8d
qshuigege <qshuigege@163.com>
2019-10-07 22:49:55 +0800

    aaa

--- erp/src/main/java/com/company/erp/config/WebMvcConfigurerImpl.java
+++ erp/src/main/java/com/company/erp/config/WebMvcConfigurerImpl.java
@@ -20,10 +20,10 @@ public class WebMvcConfigurerImpl implements WebMvcConfigurer {
                         "/**"
                 )//
                 .excludePathPatterns(
-                        "/static/**",//
-                        "/test/**",//
                         "/error",//
-                        "/login"
+                        "/static/**",//
+                        "/login",//
+                        "/test/**"
                 );
     }
 }
--- erp/src/main/java/com/company/erp/controller/UserController.java
+++ erp/src/main/java/com/company/erp/controller/UserController.java
@@ -4,13 +4,21 @@ import com.fs.diyutils.JsonResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     @RequestMapping("/getAllUser")
-    public JsonResponse getAllUser(){
-        return JsonResponse.success("test getAllUser");
+    public JsonResponse getAllUser(HttpServletRequest request){
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("msg", "test get all user");
+        map.put("remoteAddr", request.getRemoteAddr());
+        map.put("remoteHost", request.getRemoteHost());
+        map.put("remotePort", request.getRemotePort());
+        return JsonResponse.success(map);
     }
 
 }
